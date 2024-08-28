c265d3deb6bb5ef64058c3409ee0eb10f077bc80
Luis Bernabe <160785873+LuisBern@users.noreply.github.com>
2024-08-28 14:11:36 -0600

    chore: base proyect created

---
+++ app/src/main/java/com/payclip/blaze/embbebed_connection/sdk/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.payclip.blaze.embbebed_connection.sdk
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.payclip.blaze.embbebed_connection.sdk.ui.theme.EmbbebedconnectionsdkTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            EmbbebedconnectionsdkTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    EmbbebedconnectionsdkTheme {
+        Greeting("Android")
+    }
+}
